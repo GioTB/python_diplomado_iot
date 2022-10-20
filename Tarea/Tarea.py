70e75aed038c6106f896af5458735fa5177a0e8b
Giovanni Troncoso Bordoni <giovanni.troncosob@gmail.com>
2022-10-20 19:14:16 -0300

    Modificaciones para correlación estado temperatura

--- Tarea/Tarea.py
+++ Tarea/Tarea.py
@@ -15,7 +15,7 @@ estadoCalefactor = False
 #
 temperaturaActual = random.randint(0, 35) #temperatura actual
 print('Temperatura inicial : {} ºC'.format(temperaturaActual))
-Log = open('log_Tinit_{}_Tcalefactor_{}_tTotal_{}_tMuestreo_{}.csv'.format(temperaturaActual,temperatureSetPoint,ventanaTiempo,tiempoMuestreo),'w')
+Log = open('log-tempinicial{}-tempcalefactor{}_tiempototal_{}-tiempomedicion_{}.csv'.format(temperaturaActual,temperatureSetPoint,ventanaTiempo,tiempoMuestreo),'w')
 
 while ellapsedTime < ventanaTiempo:
     deltaTemperatura = round(random.uniform(0, 2), 2)  # delta de temp
@@ -26,13 +26,17 @@ while ellapsedTime < ventanaTiempo:
     else:
         estadoCalefactor = False
         estadoCalefactorString = "apagado"
+
+    print("Temperatura actual:", round(temperaturaActual, 2), "con el calefactor", estadoCalefactorString)
+    Log.write('{};{}\n'.format(round(temperaturaActual, 2), estadoCalefactor))
+    
     if estadoCalefactor:
         temperaturaActual += deltaTemperatura
     else:
         temperaturaActual -= deltaTemperatura
-    print("Temperatura actual:",round(temperaturaActual,2), "con el calefactor",estadoCalefactorString)
+
     ellapsedTime += tiempoMuestreo
-    Log.write('{};{}\n'.format(round(temperaturaActual,2),estadoCalefactor))
+
     time.sleep(tiempoMuestreo)
 print('Fin de medición')
 Log.close()
